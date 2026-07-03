5c0e9ba7abd5d6d6cf784a93441976cd82dfd869
Ousama <lasrioussama@gmail.com>
2026-07-04 00:32:09 +0100

    feat: Initialize Spring Boot project with core dependencies and YAML configuration _ Add Spring Web, Data JPA, Security, Validation, DevTools _ Configure H2 database for development profile _ Set up PostgreSQL configuration for production profile _ Implement YAML-based configuration with dev profile The development profile uses H2 in-memory database with SQL logging enabled for debugging. The production profile support for secure credentials.

--- backend/src/main/java/edu/English/bac_in_hand/BacInHandApplication.java
+++ backend/src/main/java/edu/English/bac_in_hand/BacInHandApplication.java
@@ -1,13 +1,13 @@
-package com.bacInHand.demo;
+package edu.English.bac_in_hand;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class BacInHandApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(BacInHandApplication.class, args);
 	}
 
 }
--- backend/src/test/java/edu/English/bac_in_hand/BacInHandApplicationTests.java
+++ backend/src/test/java/edu/English/bac_in_hand/BacInHandApplicationTests.java
@@ -1,10 +1,10 @@
-package com.bacInHand.demo;
+package edu.English.bac_in_hand;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class BacInHandApplicationTests {
 
 	@Test
 	void contextLoads() {
